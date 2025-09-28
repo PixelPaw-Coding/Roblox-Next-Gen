2bbbb76880b7bf21dfbdb181e1cf6070fbc06e4d
PixelPaw Coding <pixelpaw-coding@keemail.me>
2025-09-28 11:23:50 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
 document.getElementById('reloadBtn').addEventListener('click', () => {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
     if (tabs.length > 0) {
+      chrome.scripting.insertCSS({
+        target: { tabId: tabs[0].id },
+        files: ['styles.css']
+      });
       chrome.tabs.reload(tabs[0].id);
+      console.log("Roblox Next Gen 2.0 applied! ⚡");
     }
   });
 });
